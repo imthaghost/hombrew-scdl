fa64472e13cdd504f422305bf7dc39a7cb475ff6
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-04 21:59:54 -0700

    Brew formula update for scdl version v2.0.0

--- Formula/scdl.rb
+++ Formula/scdl.rb
@@ -2,16 +2,16 @@
 class Scdl < Formula
   desc "SoundCloud Music Downloader | Scdl is the fastest SoundCloud music downloading CLI tool. Scdl utilizes go routine pools which allows you to download any song from SoundCloud within seconds. There are extended features such as recursively downloading all songs from a given artist and grabbing song artwork."
   homepage "https://github.com/imthaghost/scdl"
-  version "1.0.0"
+  version "2.0.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/imthaghost/scdl/releases/download/v1.0.0/scdl_1.0.0_macOS-64bit.tar.gz"
-    sha256 "b8ac8296c14b5680af414a184a3650e6559c9d2cfcac99e962f18ea5913f19c3"
+    url "https://github.com/imthaghost/scdl/releases/download/v2.0.0/scdl_2.0.0_macOS-64bit.tar.gz"
+    sha256 "5aed4e28ff65ae8a69a0e5fcc5b4ce14a519f8615ef4a4024ef2cb22b6207a8a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/imthaghost/scdl/releases/download/v1.0.0/scdl_1.0.0_Linux-64bit.tar.gz"
-      sha256 "1c54fbe26ffbf3c8114dfbadb71000536253ee8fa71da47fcb3d41035bb019ac"
+      url "https://github.com/imthaghost/scdl/releases/download/v2.0.0/scdl_2.0.0_Linux-64bit.tar.gz"
+      sha256 "5c0525385662b90ea12ca86c376b44f145778a717c7a2cfb82ce6daa39a226d9"
     end
   end
 
